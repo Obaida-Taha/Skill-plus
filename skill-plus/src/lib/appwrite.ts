a0f06fab40f1837033bd636d26b4961a8a3e3aab
obaida <oboydagoat03@gmail.com>
2026-08-03 16:22:52 +0200

    data base tweak to allow phones to test also

--- skill-plus/src/lib/appwrite.ts
+++ skill-plus/src/lib/appwrite.ts
@@ -4,7 +4,7 @@ import { Client, Account, Databases, ID } from 'react-native-appwrite';
 export const client = new Client();
 
 client
-  .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1')
+  .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT || 'https://fra.cloud.appwrite.io/v1') // Updated to fra.cloud.appwrite.io
   .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID || '6a6fbe6500378f261f00');
 
 export const account = new Account(client);
